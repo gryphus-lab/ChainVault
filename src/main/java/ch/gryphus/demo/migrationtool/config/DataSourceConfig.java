c834da4acefc0778138fcd9be7d4854a67935135
gryphus-lab <80040733+gryphus-lab@users.noreply.github.com>
2026-02-17 20:46:04 +0100

    Fix configs for jobrunr, sftp, xml

--- src/main/java/ch/gryphus/demo/migrationtool/config/DataSourceConfig.java
+++ src/main/java/ch/gryphus/demo/migrationtool/config/DataSourceConfig.java
@@ -1,11 +1,9 @@
 package ch.gryphus.demo.migrationtool.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.AutoConfiguration;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.core.env.Environment;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
