6921793ceb9a4ec6db2398a9e07c05a87e2f77cc
gryphus-lab <80040733+gryphus-lab@users.noreply.github.com>
2026-03-14 19:17:50 +0100

    Improve setup scripts

--- docker-resources/json-server/init-scripts/setup.sh
+++ docker-resources/json-server/init-scripts/setup.sh
@@ -1,19 +1,27 @@
 #!/bin/sh
+
+echo "Installing Python 3..."
 apk add python3 
 ln -sf python3 /usr/bin/python
 apk add --no-cache py3-pip
 apk add --no-cache build-base
 apk add --no-cache ttf-dejavu
 python3 -m venv .venv && . .venv/bin/activate
+echo "Python 3 installed"
 
+
+echo "Installing dependencies..."
 pip install --upgrade pip
 pip install pipreqs
 pipreqs --force /data
 pip install -r /data/requirements.txt
+echo "Dependencies installed"
+
 
 echo "Creating source data..."
 cd /data || exit 1
 python3 create_source_data.py
+echo "Source data generated"
 
 echo "Starting json-server..."
 npm install -g json-server
