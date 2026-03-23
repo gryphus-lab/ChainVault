8511e6f91c2e9252e6fadc413113377e5f5406ac
gryphus-lab <80040733+gryphus-lab@users.noreply.github.com>
2026-03-23 17:51:53 +0100

    Refactor layers for migration layer aligned to layer architecture

--- chainvault-migration/src/test/java/ch/gryphus/chainvault/arch/MigrationArchitectureTest.java
+++ chainvault-migration/src/test/java/ch/gryphus/chainvault/arch/MigrationArchitectureTest.java
@@ -5,10 +5,10 @@ package ch.gryphus.chainvault.arch;
 
 import static com.tngtech.archunit.library.Architectures.layeredArchitecture;
 
-import com.tngtech.archunit.core.domain.JavaClasses;
 import com.tngtech.archunit.core.importer.ImportOption;
 import com.tngtech.archunit.junit.AnalyzeClasses;
 import com.tngtech.archunit.junit.ArchTest;
+import com.tngtech.archunit.lang.ArchRule;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
@@ -17,32 +17,27 @@ import lombok.extern.slf4j.Slf4j;
         importOptions = ImportOption.DoNotIncludeTests.class)
 class MigrationArchitectureTest {
 
-    // TODO: Check and fix violations
     @ArchTest
-    static void layered_architecture(JavaClasses classes) {
-        var layeredArchitecture =
-                layeredArchitecture()
-                        .consideringAllDependencies()
-                        .layer("Service")
-                        .definedBy("..service..")
-                        .layer("Domain")
-                        .definedBy("..domain..")
-                        .layer("Util")
-                        .definedBy("..util..")
-                        .layer("Config")
-                        .definedBy("..config..")
-                        .whereLayer("Service")
-                        .mayNotBeAccessedByAnyLayer()
-                        .whereLayer("Domain")
-                        .mayOnlyBeAccessedByLayers("Service")
-                        .whereLayer("Util")
-                        .mayOnlyBeAccessedByLayers("Service", "Domain")
-                        .whereLayer("Config")
-                        .mayNotBeAccessedByAnyLayer();
+    static final ArchRule layered_architecture =
+            layeredArchitecture()
+                    .consideringAllDependencies()
+                    // define te layers
+                    .layer("Service")
+                    .definedBy("..service..")
+                    .layer("Config")
+                    .definedBy("..config..")
+                    .layer("Util")
+                    .definedBy("..util..")
+                    .layer("Exception")
+                    .definedBy("..exception..")
 
-        var result = layeredArchitecture.evaluate(classes);
-        if (result.hasViolation()) {
-            log.warn(String.valueOf(result.getFailureReport()));
-        }
-    }
+                    // enforce layer restrictions
+                    .whereLayer("Service")
+                    .mayNotBeAccessedByAnyLayer()
+                    .whereLayer("Config")
+                    .mayOnlyBeAccessedByLayers("Service")
+                    .whereLayer("Util")
+                    .mayOnlyBeAccessedByLayers("Service")
+                    .whereLayer("Exception")
+                    .mayOnlyBeAccessedByLayers("Service");
 }
