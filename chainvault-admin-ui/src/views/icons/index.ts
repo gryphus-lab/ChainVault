a07c3028a3a6b4e86a680bbfb5c8d594d777f1f0
gryphus-lab <80040733+gryphus-lab@users.noreply.github.com>
2026-04-06 21:40:07 +0200

    initial check in for admin ui

---
+++ chainvault-admin-ui/src/views/icons/index.ts
@@ -0,0 +1,5 @@
+import CoreUIIcons from '../icons/coreui-icons/CoreUIIcons'
+import Flags from '../icons/flags/Flags'
+import Brands from '../icons/brands/Brands'
+
+export { CoreUIIcons, Flags, Brands }
