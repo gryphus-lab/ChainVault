d0bdc10f4425ff46dff4a761ce8d490ba167ca61
gryphus-lab <80040733+gryphus-lab@users.noreply.github.com>
2026-04-16 00:24:39 +0200

    Update branding on app header

--- chainvault-admin-ui/src/components/header/AppHeaderDropdown.tsx
+++ chainvault-admin-ui/src/components/header/AppHeaderDropdown.tsx
@@ -25,7 +25,7 @@ import {
 } from '@coreui/icons'
 import CIcon from '@coreui/icons-react'
 
-import avatar8 from './../../assets/images/avatars/8.jpg'
+import avatar8 from './../../assets/images/avatars/gryphus.jpg'
 
 const AppHeaderDropdown = () => {
   return (
